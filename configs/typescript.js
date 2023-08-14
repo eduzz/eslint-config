838466c4e27415ad2e517bd90443f22b0ee4e7a1
Carlos André Ferrari <caferrari@gmail.com>
2023-08-14 14:54:40 -0300

    change to warn

--- configs/typescript.js
+++ configs/typescript.js
@@ -3,7 +3,7 @@ module.exports = {
   rules: {
     'no-unused-vars': 'off',
     '@typescript-eslint/no-unused-vars': [
-      'error',
+      'warn',
       {
         'argsIgnorePattern': '^_',
         'varsIgnorePattern': '^_',
