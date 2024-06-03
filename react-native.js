f922106058ed597ea2acb61d666d58cda7c32146
danieloprado <danielprado.ad@gmail.com>
2024-06-03 17:56:09 +0100

    feat: move react-native to a separated file

--- react-native.js
+++ react-native.js
@@ -1,3 +1,4 @@
+const configReactNative = require('./configs/react-native');
 const { configs } = require('./react');
 const { fixupPluginRules } = require('@eslint/compat');
 const pluginReactNative = require('eslint-plugin-react-native');
@@ -7,17 +8,5 @@ const ignores = require('./ignores');
 module.exports = {
   ignores,
   /** @type import('eslint').Linter.FlatConfig[] */
-  configs: [
-    ...configs,
-    {
-      name: '@eduzz/eslint-config-react-native',
-      plugins: {
-        'react-native': fixupPluginRules(pluginReactNative)
-      },
-      rules: {
-        'react-native/no-inline-styles': ['warn'],
-        'react-native/no-unused-styles': ['error']
-      }
-    }
-  ]
+  configs: [...configs, configReactNative]
 };
