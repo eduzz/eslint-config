3180131450c5396a68292c42563c0bf750c3ba21
danieloprado <danielprado.ad@gmail.com>
2024-06-03 13:58:04 +0100

    chore: remove console.log

--- ignores.js
+++ ignores.js
@@ -23,7 +23,6 @@ module.exports = function gitignore(...extra) {
 
     const parsed = parse(`${content}\n`);
     const globs = parsed.globs();
-    console.log(JSON.stringify(globs, null, 2));
 
     for (const glob of globs) {
       if (glob.type === 'ignore') ignores.push(...glob.patterns);
